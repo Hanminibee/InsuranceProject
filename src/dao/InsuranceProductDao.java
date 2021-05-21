9361bc88ba1346075fc7aa29473f1da48845e408
TaeYeongKwak <abgudv6403@naver.com>
2021-05-22 00:14:59 +0900

    insuranceProductDao first

--- src/dao/InsuranceProductDao.java
+++ src/dao/InsuranceProductDao.java
@@ -5,6 +5,7 @@ import entity.InsuranceProduct;
 public interface InsuranceProductDao {
 
 	public boolean add(InsuranceProduct insuranceProduct);
+	public boolean update(InsuranceProduct insuranceProduct);
 	public boolean delete(InsuranceProduct insuranceProduct);
 	public InsuranceProduct search(String productName);
 	
